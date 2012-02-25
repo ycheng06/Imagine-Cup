2099769e3e4ad97da12e39ae698951404dd9d098
Jason Cheng <yannru.cheng@gmail.com>
2012-02-25 00:41:51 -0500

    seed database automatically

--- TBTracker/TBTracker/Models/Seeder.cs
+++ TBTracker/TBTracker/Models/Seeder.cs
@@ -6,11 +6,27 @@ using System.Data.Entity;
 
 namespace TBTracker.Models
 {
-    public class Seeder : DropCreateDatabaseIfModelChanges<TrackerEntities>
+    public class Seeder : DropCreateDatabaseAlways<TrackerEntities>
     {
         protected override void Seed(TrackerEntities context)
         {
-            context.SaveChanges();
+            Patient patient = new Patient
+            {
+                FirstName = "Jason",
+                LastName = "Cheng",
+                Phone = "6175129381",
+                FamilyPhone1 = "6175129381",
+                FamilyPhone2 = "6175129381",
+                Address = "887 Broadway",
+            };
+            Alert alert = new Alert
+            {
+                AlertDate = DateTime.Now,
+                AlertType = "Miss Medication",
+                Patient = patient,
+            };
+            context.Patients.Add(patient);
+            context.Alerts.Add(alert);
         }
     }
 }

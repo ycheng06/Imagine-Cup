5262fecb6a808ceb74524f280da740ec92963ad4
Wenshiang Chung <aliken9@gmail.com>
2012-02-25 02:37:08 -0500

    bare backbone for timeline

---
+++ TBTracker/TBTracker/Controllers/TimelineController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TBTracker.Models;
+
+namespace TBTracker.Controllers
+{
+    public class TimelineController : Controller
+    {
+        TrackerEntities trackerDB = new TrackerEntities();
+
+        //
+        // GET: /Timeline/
+
+        public ActionResult Index()
+        {
+            var patients = trackerDB.Patients.ToList();
+            return View(patients);
+        }
+
+/*        public ActionResult Form()
+        {
+
+        }*/
+    }
+}

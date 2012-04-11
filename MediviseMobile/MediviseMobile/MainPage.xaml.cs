cd055d898ba67eb55c87f4b4c95989119e71cf5e
Jason Cheng <yannru.cheng@gmail.com>
2012-04-11 02:05:34 +0100

    alerts, patients, profile finished
    to-do: drugs, tests, messages (make them editable)

--- MediviseMobile/MediviseMobile/MainPage.xaml.cs
+++ MediviseMobile/MediviseMobile/MainPage.xaml.cs
@@ -16,16 +16,6 @@ namespace MediviseMobile
 {
     public partial class MainPage : PhoneApplicationPage
     {
-
-        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
-        {
-            if (!App.ViewModel.IsDataLoaded)
-            {
-                App.ViewModel.LoadData();
-            }
-            base.OnNavigatedTo(e);
-        }
-
         // Constructor
         public MainPage()
         {
@@ -33,7 +23,55 @@ namespace MediviseMobile
 
             // Set the data context of the listbox control to the sample data
             DataContext = App.ViewModel;
+            //this.Loaded += new RoutedEventHandler(MainPage_Loaded);
         }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            if(!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadAlert();
+            }
+        }
+
+        //load pivot on demand
+        private void PivotSelectionChanged(Object sender, SelectionChangedEventArgs e)
+        {
+            Debug.WriteLine(Pivot1.SelectedIndex.ToString());
+            switch (Pivot1.SelectedIndex)
+            {
+                case 0:
+                    App.ViewModel.LoadAlert();
+                    break;
+                case 1:
+                    App.ViewModel.LoadPatient();
+                    break;
+            }
+        }
+
+        //Handle selection changed on ListBox
+        private void SecondListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //If selected index is -1 (no selection) do nothing
+            if (SecondListBox.SelectedIndex == -1) return;
+
+            int id = App.ViewModel.Patients[SecondListBox.SelectedIndex].PatientId;
+            string firstName = App.ViewModel.Patients[SecondListBox.SelectedIndex].FirstName;
+            string lastName = App.ViewModel.Patients[SecondListBox.SelectedIndex].LastName;
+            //Navigate to new page
+            NavigationService.Navigate(new Uri("/PatientPage.xaml?id=" + id + 
+                "&firstName=" + firstName + "&lastName=" + lastName, UriKind.Relative));
+
+            //Reset selected index to -1 (no selection)
+            SecondListBox.SelectedIndex = -1;
+        }
+
+        private void AppBarRefresh_Click(object sender, EventArgs e)
+        {
+            //Reload data from OData Service
+            App.ViewModel.Refresh();
+        }
+
 
     }
 }
---
+++ MediviseMobile/MediviseMobile/ViewModels/PatientViewModel.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Services.Client;
+using System.Linq;
+using System.Windows;
+using MediviseMobile.Medivise;
+using System.Diagnostics;
+
+namespace MediviseMobile
+{
+    public class PatientViewModel : INotifyPropertyChanged
+    {
+        private static Uri rootUri =
+            new Uri("http://eosimaginecup.cloudapp.net/MediviseService.svc/");
+
+        private MediviseEntities context = null;
+
+        private DataServiceCollection<Patient> patients;
+
+        public DataServiceCollection<Patient> Patients
+        {
+            get { return patients; }
+
+            private set
+            {
+                patients = value;
+
+                //Register a handler for LoadCompleted callback
+                patients.LoadCompleted += OnPatientsLoaded;
+
+                //Raise the PropertyChanged evetns.
+                NotifyPropertyChanged("Patients");
+            }
+        }
+
+        public bool IsDataLoaded { get; private set; }
+
+        public void LoadPatient(int patientId)
+        {
+            Debug.WriteLine("loading Data");
+            //Instantiate the context and binding collection.
+            context = new MediviseEntities(rootUri);
+            Patients = new DataServiceCollection<Patient>(context);
+
+            var query = from p in context.Patients
+                        where p.PatientId == patientId
+                        select p;
+            Debug.WriteLine(query.ToString());
+            Patients.LoadAsync(query);
+        }
+
+        public void LoadData(MediviseEntities c, DataServiceCollection<Patient> patients)
+        {
+            context = c;
+            Patients = patients;
+
+            IsDataLoaded = true;
+        }
+
+        private void OnPatientsLoaded(object sender, LoadCompletedEventArgs e)
+        {
+            //Make sure that we load all pages of the Customers feed.
+            Debug.WriteLine("patient loaded");
+            if (Patients.Continuation != null)
+            {
+                Patients.LoadNextPartialSetAsync();
+            }
+          
+            IsDataLoaded = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            var propertyChanged = PropertyChanged;
+            if (propertyChanged != null)
+            {
+                propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        /*
+        * Application bar functions
+        */
+        public void Refresh(int id)
+        {
+            MergeOption cachedOption = context.MergeOption;
+            context.MergeOption = MergeOption.OverwriteChanges;
+
+            //Reload data 
+            this.LoadPatient(id);
+
+            //Reset the merge option
+            context.MergeOption = cachedOption;
+        }
+
+        public void SaveChanges()
+        {
+            this.context.BeginSaveChanges(OnChangesSaved, this.context);
+        }
+
+        private void OnChangesSaved(IAsyncResult result)
+        {
+            //use the Dispatcher to ensure that the 
+            //asynchronous call returns in the correct thread.
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+                {
+                    this.context = result.AsyncState as MediviseEntities;
+
+                    try
+                    {
+                        //complete the save changes operation
+                        this.context.EndSaveChanges(result);
+                        MessageBox.Show("saved");
+                    }
+                    catch (DataServiceRequestException ex)
+                    {
+                        MessageBox.Show("fail to save");
+                    }
+                });
+        }
+    }
+}
